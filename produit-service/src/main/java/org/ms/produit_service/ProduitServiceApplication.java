56f6722908dc450338c9fa9806857b15f9bb16e7
NourCharfi <nourcharfi58@gmail.com>
2025-05-28 20:29:08 +0100

    HHHHHHHHHHHHHHHHH

--- produit-service/src/main/java/org/ms/produit_service/ProduitServiceApplication.java
+++ produit-service/src/main/java/org/ms/produit_service/ProduitServiceApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+
 
 @SpringBootApplication
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class ProduitServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProduitServiceApplication.class, args);
